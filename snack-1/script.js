05315f0e994a53b4e7ca62b62eee233c4173db9b
severogianluca <gianlucasevero300@gmail.com>
2025-03-12 17:44:52 +0100

    Feat: snack1

---
+++ snack-1/script.js
@@ -0,0 +1,19 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const  result = names.forEach((element) =>{ 
+    console.log(element)
+})
+
+
+
+
+for (let i = 0; i < names.length; i++) {
+    let list;
+    list = names[i];
+    console.log(list);
+
+}
+
