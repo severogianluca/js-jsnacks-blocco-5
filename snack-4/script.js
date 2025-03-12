e759f2b2c955c331a8dc0cae62c486a3839f8ef2
severogianluca <gianlucasevero300@gmail.com>
2025-03-12 18:05:49 +0100

    Feat: snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const autoreName = posts.map((element) => element.author)
+  console.log(autoreName)
+
