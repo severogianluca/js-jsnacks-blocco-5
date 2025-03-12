93fea804c57ef2b110fa878d3eb3a276da3ab8ba
severogianluca <gianlucasevero300@gmail.com>
2025-03-12 18:13:23 +0100

    Feat: snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const pari = nums.filter((element) => {
+    return element % 2 === 0
+})
+console.log(pari)
