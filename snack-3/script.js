986a79557a04e0cbf2ef1be5649bbaec6eece3ae
severogianluca <gianlucasevero300@gmail.com>
2025-03-12 17:55:27 +0100

    Feat: snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,8 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numberPlusOne = numbers.map((element) => {
+    return element + 1;
+})
+console.log(numberPlusOne)
